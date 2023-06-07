68384551f52064b5054dfed4dc94afc4c2951536
Atalay Özmen <Atalay>
2023-06-07 20:39:05 +0200

    migrate code to template

--- src/index.ts
+++ src/index.ts
@@ -10,65 +10,67 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
-
-  res.json(todos);
+app.get('/', async (req, res) => {
+  const techRadarElements = await prisma.techRadarElement.findMany();
+  res.json(techRadarElements);
 });
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
+app.post('/', async (req, res) => {
+  const { label, quadrant, ring, active, moved } = req.body;
+  const techRadarElement = await prisma.techRadarElement.create({
     data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
+      id: Date.now(),
+      label,
+      quadrant,
+      ring,
+      active,
+      moved,
     },
   });
-
-  return res.json(todo);
+  res.json(techRadarElement);
 });
 
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
+app.post('/addmany', async (req, res) => {
+  const techRadarElements= req.body;
+
+  try {
+  const createdTechRadarElements = await prisma.techRadarElement.createMany({
+    data: techRadarElements,
+    skipDuplicates: true,
   });
 
-  return res.json(todo);
+  res.json(createdTechRadarElements);
+  } catch (e) {
+    console.log("error: ", e)
+  }
 });
 
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
 
-  return res.json(todo);
-});
+app.put('/saveEntries', async (req, res) => {
 
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
+  const techRadarElements = req.body;
+ 
+  try{
+    console.log("testest")
+    const deletedTechRadarElements = await prisma.techRadarElement.deleteMany({});
+  }
+  catch(e){
+    console.log("saveentries error: Delete all failed: ")
+  }
 
-  return res.send({ status: "ok" });
-});
+  try{
+    console.log("test: ", techRadarElements)
+    const createdTechRadarElements = await prisma.techRadarElement.createMany({
+      data: techRadarElements,
+      skipDuplicates: true,
+    });
 
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Todo REST API</h1>
-  <h2>Available Routes</h2>
-  <pre>
-    GET, POST /todos
-    GET, PUT, DELETE /todos/:id
-  </pre>
-  `.trim(),
-  );
+    res.json(createdTechRadarElements);
+  
+  } catch (e) {
+    console.log("saveEntries error: ", e)
+  }
+
 });
 
 app.listen(Number(port), "0.0.0.0", () => {
